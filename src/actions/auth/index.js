834306a2b901f36ecf489a3bde5301d4ea6bd725
Gabriel Bento Lisboa <gabrielbento.rx@outlook.com>
2019-04-05 06:50:52 -0300

    Save token on async storage

--- src/actions/auth/index.js
+++ src/actions/auth/index.js
@@ -1,9 +1,11 @@
 import { LOGIN, ERROR } from './types'
 import api from '../../utils/api'
+import { AsyncStorage } from 'react-native'
 
 export const login = (email, password) => async dispatch => {
   try {
     const { data } = await api.post('/login', { email, password })
+    await AsyncStorage.setItem('@Auth:Token', data.token)
     dispatch({
       type: LOGIN,
       token: data.token
